e41460e7174106b3561d2d2b608404bd2f7b2751
patitapaban <patitapabanpanda046@gmail.com>
2026-01-09 18:16:01 +0530

    revert changes

--- src/main/java/com/testing/testing/entity/A.java
+++ src/main/java/com/testing/testing/entity/A.java
@@ -4,10 +4,6 @@ public class A {
     public static void main(String[] args) {
         int x = 30;
 
-        int y = 2000;
-        String z = "300";
-        float f = 20.3f;
-        double d = 20.0d;
 
 
     }
